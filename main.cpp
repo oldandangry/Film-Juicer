6321234a7c2a9de32b17bac23fe2d3ba02522be6
oldandangry <alexitq@protonmail.com>
2025-12-29 20:11:48 +0100

    Reverted to previous version, before mip-map grain.

--- main.cpp
+++ main.cpp
@@ -640,7 +640,7 @@ void JuicerPluginFactory::describeInContext(OFX::ImageEffectDescriptor& desc, OF
             OFX::DoubleParamDescriptor* p = desc.defineDoubleParam(JuicerParams::kGrainParticleScaleMaster);
             p->setLabel("Particle scale (M)");
             p->setHint("Master control for particle scale; adjusts RGB values together.");
-            p->setDefault(1.826);
+            p->setDefault(1.48);
             p->setRange(0.0, 10.0);
             p->setDisplayRange(0.0, 3.0);
             if (grpGrain) p->setParent(*grpGrain);
@@ -760,7 +760,7 @@ void JuicerPluginFactory::describeInContext(OFX::ImageEffectDescriptor& desc, OF
             OFX::Double3DParamDescriptor* p = desc.defineDouble3DParam(JuicerParams::kGrainParticleScale);
             p->setLabel("Particle scale");
             p->setHint("Scale of particle area for the RGB layers (multiplies particle area).");
-            p->setDefault(1.359, 1.559, 2.559);
+            p->setDefault(1.10, 1.27, 2.08);
             p->setRange(0.0, 0.0, 0.0, 10.0, 10.0, 10.0);
             p->setDisplayRange(0.0, 0.0, 0.0, 3.0, 3.0, 3.0);
             p->setDimensionLabels("R", "G", "B");
